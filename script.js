0a1533ae43833a885f3faa27662c9c8fab684234
raul763 <urbinaestuardo62@gmail.com>
2025-03-06 23:16:23 -0600

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,37 @@ document.querySelectorAll('.view-btn').forEach(button => {
         alert('Redirigiendo a la página del hotel...');
     });
 });
+
+function abrirMapa(url) { /*PARA ABRIR LA PAGINA DE GOOGLE MAPS */
+    window.open(url, '_blank');
+}
+
+/*MARQUESINA */
+document.addEventListener("DOMContentLoaded", function () {
+    let texto = document.getElementById("marquesina");
+    let posicion = window.innerWidth;
+
+    function moverMarquesina() {
+        posicion -= 2.7; // Ajusta la velocidad cambiando este valor
+        if (posicion < -texto.offsetWidth) {
+            posicion = window.innerWidth;
+        }
+        texto.style.transform = `translateX(${posicion}px)`;
+        requestAnimationFrame(moverMarquesina);
+    }
+
+    moverMarquesina();
+});
+// Asegúrate de que el contador de visitas se actualice correctamente
+let visitCount = localStorage.getItem('visitCount') || 0;
+document.getElementById('visitCount').innerText = visitCount;
+
+function updateVisitCount() {
+  visitCount++;
+  localStorage.setItem('visitCount', visitCount);
+  document.getElementById('visitCount').innerText = visitCount;
+}
+
+// Llama a la función cuando la página se cargue
+window.onload = updateVisitCount;
+
